6eba8a5479e0ba3f19ed3f5a19346642e1919f86
shubiisha <shubiisha23@gmail.com>
2026-06-09 19:53:50 +0530

    Enable installable web app functionality

--- script.js
+++ script.js
@@ -533,6 +533,14 @@ window.onload = function () {
   });
 
   requestNotificationPermission();
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) =>
+        console.error("Service Worker Registration Failed:", err),
+      );
+  }
 };
 
 window.addTask = addTask;
